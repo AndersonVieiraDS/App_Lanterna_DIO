c0717ea42b2f667e0990b8e4f115e7822780db78
Hypnotic <anderson.mfvs@hotmail.com>
2022-06-27 07:44:08 -0300

    main

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import {View, StyleSheet, Image } from 'react-native';
-import Imagex from './assets/Icons_Dio/eco-light-off.png'
+import React, {useState} from 'react';
+import {View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 const App = () => {
-  const toggle = true; //false
+  const [toggle, setToggle] = useState(false);
 
   // if toggle return light
   return (
-  <View style={toggle ? style.containerLight : style.container}>
-  <Image source={Imagex} />
+    <View style={toggle ? style.containerLight : style.container}>
+      <TouchableOpacity
+        onPress={() =>{
+          setToggle(true);
+        }}>
+        <Image
+          style={toggle ? style.lightingOn : style.lightingOff}
+          source={
+            toggle
+             ? require('./assets/Icons_Dio/eco-light.png')
+             : require('./assets/Icons_Dio/eco-light-off.png')
+          } 
+        />
+      <Image
+        style={style.logoDio}
+        source={
+          toggle
+           ? require('./assets/Icons_Dio/logo-dio.png')
+           : require('./assets/Icons_Dio/logo-dio-white.png')
+        }
+      />
+    </TouchableOpacity>
   </View>
   );
 };
@@ -27,5 +46,24 @@ const style = StyleSheet.create({
     backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  lightingOn:{
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 150,
+    height: 150,
+  },
+  lightingOff:{
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    tintColor: 'white',
+    width: 150,
+    height: 150,
+  },
+  logoDio:{
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 250,
+    height: 250,
   },
 });
